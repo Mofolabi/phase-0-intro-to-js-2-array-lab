c7a6794d41f6aec4cc23fcf716ac164071466613
Mofolabi Osotimehin <mofolabi.osotimehin@student.moringaschool.com>
2023-01-18 15:16:56 +0100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ['Milo','Otis','Garfield']
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
+}
+function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    let cats2 = [...cats]
+    cats2.push(name)
+    return cats2 
+}
+function prependCat(name){
+    let cats2 = [...cats]
+    cats2.unshift(name)
+    return cats2
+}
+function removeLastCat(){
+    let cats2 = [...cats]
+    cats2.pop()
+    return cats2
+}
+function removeFirstCat(){
+    let cats2 = [...cats]
+    cats2.shift()
+    return cats2
+}
